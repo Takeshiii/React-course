f013e3cec9a2ef1b664574fca8b4156f0e6483d6
Takeshi <takeshiii360@gmail.com>
2023-09-28 17:20:28 +0900

    changes after code review

--- src/main.jsx
+++ src/main.jsx
@@ -11,36 +11,44 @@ root.render(
       <div>
         <h1>{restaurants[0].name}</h1>
         <h2>Menu</h2>
-        <div>
-          {restaurants[0].menu.map((dish, index) => {
-            return (
-              <ul key={index}>
-                <li>name: {dish.name}</li>
-                <li>price: {dish.price}</li>
-                <li>
-                  ingredients:
-                  {dish.ingredients.map((Ingredient, index) => {
-                    return <ol key={index}>- {Ingredient}</ol>;
-                  })}
-                </li>
-              </ul>
-            );
-          })}
-        </div>
+        <ul>
+          <li>Name: {restaurants[0].menu[0].name}</li>
+          <li>Price: {restaurants[0].menu[0].price}</li>
+          <li>
+            Ingredients: {restaurants[0].menu[0].ingredients[0]},{" "}
+            {restaurants[0].menu[0].ingredients[1]}
+          </li>
+        </ul>
+        <ul>
+          <li>Name: {restaurants[0].menu[1].name}</li>
+          <li>Price: {restaurants[0].menu[1].price}</li>
+          <li>Ingredients: {restaurants[0].menu[1].ingredients}</li>
+        </ul>
+        <ul>
+          <li>Name: {restaurants[0].menu[2].name}</li>
+          <li>Price: {restaurants[0].menu[2].price}</li>
+          <li>
+            Ingredients: {restaurants[0].menu[2].ingredients[0]},{" "}
+            {restaurants[0].menu[2].ingredients[1]}
+          </li>
+        </ul>
       </div>
       <div>
         <h2>Reviews</h2>
         <div>
-          {restaurants[0].reviews.map((review, index) => {
-            return (
-              <div key={index}>
-                <p>{review.user}</p>
-                <p>{review.text}</p>
-                <p>{review.rating}</p>
-                <br></br>
-              </div>
-            );
-          })}
+          <p>{restaurants[0].reviews[0].user}</p>
+          <p>
+            <em>{restaurants[0].reviews[0].text}</em>
+          </p>
+          <p>Rating: {restaurants[0].reviews[0].rating}</p>
+        </div>
+        <br></br>
+        <div>
+          <p>{restaurants[0].reviews[1].user}</p>
+          <p>
+            <em>{restaurants[0].reviews[1].text}</em>
+          </p>
+          <p>Rating: {restaurants[0].reviews[1].rating}</p>
         </div>
       </div>
     </div>
@@ -48,36 +56,50 @@ root.render(
       <div>
         <h1>{restaurants[1].name}</h1>
         <h2>Menu</h2>
-        <div>
-          {restaurants[1].menu.map((dish, index) => {
-            return (
-              <ul key={index}>
-                <li>name: {dish.name}</li>
-                <li>price: {dish.price}</li>
-                <li>
-                  ingredients:
-                  {dish.ingredients.map((Ingredient, index) => {
-                    return <ol key={index}>- {Ingredient}</ol>;
-                  })}
-                </li>
-              </ul>
-            );
-          })}
-        </div>
+        <ul>
+          <li>Name: {restaurants[1].menu[0].name}</li>
+          <li>Price: {restaurants[1].menu[0].price}</li>
+          <li>
+            Ingredients: {restaurants[1].menu[0].ingredients[0]},{" "}
+            {restaurants[1].menu[0].ingredients[1]},{" "}
+            {restaurants[1].menu[0].ingredients[2]}
+          </li>
+        </ul>
+        <ul>
+          <li>Name: {restaurants[1].menu[1].name}</li>
+          <li>Price: {restaurants[1].menu[1].price}</li>
+          <li>
+            Ingredients: {restaurants[1].menu[1].ingredients[0]},{" "}
+            {restaurants[1].menu[1].ingredients[1]},{" "}
+            {restaurants[1].menu[1].ingredients[2]},{" "}
+            {restaurants[1].menu[1].ingredients[3]}
+          </li>
+        </ul>
       </div>
       <div>
         <h2>Reviews</h2>
         <div>
-          {restaurants[1].reviews.map((review, index) => {
-            return (
-              <div key={index}>
-                <p>{review.user}</p>
-                <p>{review.text}</p>
-                <p>{review.rating}</p>
-                <br></br>
-              </div>
-            );
-          })}
+          <p>{restaurants[1].reviews[0].user}</p>
+          <p>
+            <em>{restaurants[1].reviews[0].text}</em>
+          </p>
+          <p>Rating: {restaurants[1].reviews[0].rating}</p>
+        </div>
+        <br></br>
+        <div>
+          <p>{restaurants[1].reviews[1].user}</p>
+          <p>
+            <em>{restaurants[1].reviews[1].text}</em>
+          </p>
+          <p>Rating: {restaurants[1].reviews[1].rating}</p>
+        </div>
+        <br></br>
+        <div>
+          <p>{restaurants[1].reviews[2].user}</p>
+          <p>
+            <em>{restaurants[1].reviews[2].text}</em>
+          </p>
+          <p>Rating: {restaurants[1].reviews[2].rating}</p>
         </div>
       </div>
     </div>
@@ -85,36 +107,33 @@ root.render(
       <div>
         <h1>{restaurants[2].name}</h1>
         <h2>Menu</h2>
-        <div>
-          {restaurants[2].menu.map((dish, index) => {
-            return (
-              <ul key={index}>
-                <li>name: {dish.name}</li>
-                <li>price: {dish.price}</li>
-                <li>
-                  ingredients:
-                  {dish.ingredients.map((Ingredient, index) => {
-                    return <ol key={index}>- {Ingredient}</ol>;
-                  })}
-                </li>
-              </ul>
-            );
-          })}
-        </div>
+        <ul>
+          <li>Name: {restaurants[2].menu[0].name}</li>
+          <li>Price: {restaurants[2].menu[0].price}</li>
+          <li>Ingredients: {restaurants[2].menu[0].ingredients[0]}</li>
+        </ul>
+        <ul>
+          <li>Name: {restaurants[2].menu[1].name}</li>
+          <li>Price: {restaurants[2].menu[1].price}</li>
+          <li>
+            Ingredients: {restaurants[2].menu[1].ingredients[0]},{" "}
+            {restaurants[2].menu[1].ingredients[1]}
+          </li>
+        </ul>
+        <ul>
+          <li>Name: {restaurants[2].menu[2].name}</li>
+          <li>Price: {restaurants[2].menu[2].price}</li>
+          <li>Ingredients: {restaurants[2].menu[2].ingredients[0]}</li>
+        </ul>
       </div>
       <div>
         <h2>Reviews</h2>
         <div>
-          {restaurants[2].reviews.map((review, index) => {
-            return (
-              <div key={index}>
-                <p>{review.user}</p>
-                <p>{review.text}</p>
-                <p>{review.rating}</p>
-                <br></br>
-              </div>
-            );
-          })}
+          <p>{restaurants[2].reviews[0].user}</p>
+          <p>
+            <em>{restaurants[2].reviews[0].text}</em>
+          </p>
+          <p>Rating: {restaurants[2].reviews[0].rating}</p>
         </div>
       </div>
     </div>
@@ -122,36 +141,36 @@ root.render(
       <div>
         <h1>{restaurants[3].name}</h1>
         <h2>Menu</h2>
-        <div>
-          {restaurants[3].menu.map((dish, index) => {
-            return (
-              <ul key={index}>
-                <li>name: {dish.name}</li>
-                <li>price: {dish.price}</li>
-                <li>
-                  ingredients:
-                  {dish.ingredients.map((Ingredient, index) => {
-                    return <ol key={index}>- {Ingredient}</ol>;
-                  })}
-                </li>
-              </ul>
-            );
-          })}
-        </div>
+        <ul>
+          <li>Name: {restaurants[3].menu[0].name}</li>
+          <li>Price: {restaurants[3].menu[0].price}</li>
+          <li>Ingredients: {restaurants[3].menu[0].ingredients[0]}</li>
+        </ul>
+        <ul>
+          <li>Name: {restaurants[3].menu[1].name}</li>
+          <li>Price: {restaurants[3].menu[1].price}</li>
+          <li>
+            Ingredients: {restaurants[3].menu[1].ingredients[0]},{" "}
+            {restaurants[3].menu[1].ingredients[1]}
+          </li>
+        </ul>
       </div>
       <div>
         <h2>Reviews</h2>
         <div>
-          {restaurants[3].reviews.map((review, index) => {
-            return (
-              <div key={index}>
-                <p>{review.user}</p>
-                <p>{review.text}</p>
-                <p>{review.rating}</p>
-                <br></br>
-              </div>
-            );
-          })}
+          <p>{restaurants[3].reviews[0].user}</p>
+          <p>
+            <em>{restaurants[3].reviews[0].text}</em>
+          </p>
+          <p>Rating: {restaurants[3].reviews[0].rating}</p>
+        </div>
+        <br></br>
+        <div>
+          <p>{restaurants[3].reviews[1].user}</p>
+          <p>
+            <em>{restaurants[3].reviews[1].text}</em>
+          </p>
+          <p>Rating: {restaurants[3].reviews[1].rating}</p>
         </div>
       </div>
     </div>
